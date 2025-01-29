205b79c0cc0771492be931f8b109d273cdc69919
HasnatChahad <hasnatchahad@gmail.com>
2025-01-29 11:37:05 +0600

    add cart quantity

--- javascript-amazon-project/javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project/javascript-amazon-project-main/scripts/amazon.js
@@ -60,9 +60,7 @@ products.forEach((product)=>{
             Add to Cart
           </button>
     </div>
-    `;
-   
-    
+    `; 
 
 });
 
@@ -89,9 +87,16 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         productId : productId,
         quantity : 1
         });
-      }     
-          console.log(cart);
-    });
+      } 
+     
+      let cartQuantity = 0;
+      cart.forEach((item)=>{
+        cartQuantity += item.quantity;
+      });
+  
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+      
+    }); 
 
 });
 
